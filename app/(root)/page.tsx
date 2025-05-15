0081cdecdc077f5cb608c13bd50be592b5b566d3
Jugernaut73 <37919117+Jugernaut73@users.noreply.github.com>
2025-05-14 21:59:00 -0400

    Added StartupCard.tsx component allowing for startups to be listed on the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,23 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams }: {
     searchParams: Promise<{ query?: string}>
 })  {
 
     const query = (await searchParams).query;
+
+    const posts = [{
+            _createdAt: new Date(),
+            views: 55,
+            author: { _id: 1, name: 'Adrian' },
+            _id: 1,
+            description: 'This is a description',
+            image: 'https://cacm.acm.org/wp-content/uploads/2025/01/010325.News_.Can-LLMs-Make2-G.jpg',
+            category: 'Robots',
+            title: 'We Robots'
+        }];
+
   return (
     <>
         <section className={"pink_container"}>
@@ -15,6 +28,22 @@ export default async function Home({ searchParams }: {
             </p>
 
             <SearchForm query={query} />
+        </section>
+
+        <section className="section_container">
+            <p className="text-30-semibold">
+                {query ? `Search results for "${query}"`: 'All Startups'}
+            </p>
+
+            <ul className="mt-7 card_grid">
+                {posts?.length > 0 ? (
+                    posts.map((post: StartupCardType, index: number) => (
+                        <StartupCard key={post?._id} post={post}/>
+                    ))
+                ) : (
+                    <p className="no-results">No startups found</p>
+                )}
+            </ul>
         </section>
     </>
   );
